4855adc730816dae21769d692efd9ea676953704
amandinhacruz <amandaellem2023@gmail.com>
2024-02-26 14:58:35 -0300

    adicionando crud de postagens

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,22 @@
-import { useContext } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../contexts/AuthContext"
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
 
 
 
 function Navbar() {
-  const navigate = useNavigate()
+  let navigate = useNavigate()
 
-  const { usuario , handleLogout } = useContext(AuthContext)
+  const { usuario, handleLogout } = useContext(AuthContext)
 
   function logout() {
       handleLogout()
       alert('Usuário deslogado com sucesso')
       navigate('/login')
   }
+
+  let navbarComponent
+  
 
   return (
     <>
@@ -22,12 +25,12 @@ function Navbar() {
           <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
             <div className='flex gap-4'>
-            <div className='hover:underline'>Postagens</div>
+            <Link to='/postagens' className='hover:underline'>Postagens</Link>
             <Link to='/temas' className='hover:underline'>Temas</Link>
             <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
               <div className='hover:underline'>Perfil</div>
               <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-
+             
             </div>
           </div>
         </div>
